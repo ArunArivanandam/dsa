86fa54c7e9522a1668cd7a7402bd3e1c4fe08b57
ArunArivanandam <88358701+ArunArivanandam@users.noreply.github.com>
2026-03-04 16:20:14 +0530

    second sum

--- Sum002.js
+++ Sum002.js
@@ -1,6 +1,4 @@
-const numsArray = [2, 4, 6, 3, 35, 8, 14, 24, 16];
-
-// console.log(numsArray.toSorted((a, b) => a - b));
+const numsArray = [2, 4, 6, 3, 35, 8, 14, 24, 16, 4];
 
 // Brute force- Time complexity: O(n^2)
 // function findDuplicate(nums) {
@@ -18,20 +16,30 @@ const numsArray = [2, 4, 6, 3, 35, 8, 14, 24, 16];
 
 // Optimized solution- Time Complexity: O(nlog(n))
 
-function findDuplication2(nums) {
-  const sortedNums = nums.toSorted((a, b) => a - b);
-  for (i = 0; i < sortedNums.length; i++) {
-    if (sortedNums[i] === sortedNums[i + 1]) {
+// function findDuplication2(nums) {
+//   const sortedNums = nums.toSorted((a, b) => a - b);
+//   for (i = 0; i < sortedNums.length; i++) {
+//     if (sortedNums[i] === sortedNums[i + 1]) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+
+// console.log(findDuplication2(numsArray));
+
+// More Optimized solution- Time complexity- o(n) Space complexity- o(n)
+
+function findDuplication3(nums) {
+  const map = {};
+
+  for (let num of nums) {
+    if (map[num] !== undefined) {
       return true;
     }
+    map[num] = num;
   }
   return false;
 }
 
-console.log(findDuplication2(numsArray));
-
-// More Optimized solution-
-
-function findDuplicate3(nums) {
-  const maps = {};
-}
+console.log(findDuplication3(numsArray));
